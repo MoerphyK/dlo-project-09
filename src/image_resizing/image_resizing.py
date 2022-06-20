ba4d2ae892ba0d9a125e3088efe7ad343acd4845
Christian Michael Schneider <christian-michael.schneider@dguv.de>
2022-06-20 19:16:02 +0200

    Jedes Bild wird um 4 mal 90 gedreht, dann zugeschnitten, skaliert und abgespeichert

--- src/image_resizing/image_resizing.py
+++ src/image_resizing/image_resizing.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+from pathlib import Path
 
 """
 Usage:
@@ -7,6 +8,9 @@ All images (that PIL can read) in the folder "images_raw" are assumed to be in a
 These images will be cropped to a 3:2 ratio and are then scaled to 300x200 pixels.
 The resulting images are saved in the folder "images_resized".
 """
+
+# https://pillow.readthedocs.io/en/stable/handbook/concepts.html#coordinate-system
+# https://pillow.readthedocs.io/en/stable/reference/Image.html#the-image-class
 
 LOAD_FOLDER = "images_raw"
 SAVE_FOLDER = "images_resized"
@@ -16,21 +20,23 @@ if not os.path.exists(SAVE_FOLDER):
 
 for filename in os.listdir(LOAD_FOLDER):
     load_path = os.path.join(LOAD_FOLDER, filename)
-    save_path = os.path.join(SAVE_FOLDER, filename)
-
     im = Image.open(load_path)
-    width, height = im.size
 
+    width, height = im.size
     offset = 2/3 * height / 2 / 2
     left = 0
     top = offset
     right = width
     bottom = height-offset
 
-    # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#coordinate-system
-    im_cropped = im.crop((left, top, right, bottom)) # (left, upper, right, lower)
+    for i in range (0,4):
+        im_rotated = im.rotate(i*90)
+        im_cropped = im_rotated.crop((left, top, right, bottom))
+        im_resized = im_cropped.resize((300, 200))
 
-    # https://pillow.readthedocs.io/en/stable/reference/Image.html#the-image-class
-    im_resized = im_cropped.resize((300, 200))
+        new_filename = Path(filename).stem + "_" + str(i * 90) + ".jpg"     # add degree of rotation to filename
+        save_path = os.path.join(SAVE_FOLDER, new_filename)
+        im_resized.save(save_path)
+        print(new_filename)
 
-    im_resized.save(save_path)
+print("\nFinished")

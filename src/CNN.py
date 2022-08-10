1c59f83bd387c526668377e197a0f4ac072ed5c2
Marvin Kraus <Marvin.Kraus@hotmail.de>
2022-08-10 16:33:29 +0200

    Deleted Dataloader.py + Fixed Accuracy + Fixed Eval + Auto Dataloader + added Early Stopping

--- src/CNN.py
+++ src/CNN.py
@@ -4,10 +4,12 @@
 
 ### IMPORTS LIB ###
 
+from cgi import test
 from matplotlib import image
 import numpy as np
 import pandas as pd
 import math
+import time
 
 import torch
 from torch.utils.data import DataLoader
@@ -21,47 +23,78 @@ import matplotlib.pyplot as plt
 from torchinfo import summary
 
 ### Try Runtime on CUDA ###
+#torch.cuda.is_available = lambda : False # If GPU is found but CPU should be used instead
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-### Load Data ### 
-
-# train_dataset, test_datset, train_loader, test_loader = dl.import_data()
+print( f"device: {device}" )
 
 #Hyperparameters
-num_epochs = 5
-batch_size = 5
-learning_rate = 0.001
+num_epochs = 20
+batch_size = 32
+learning_rate = 0.0005
 
-# Transforming Input into tensors for CNN usage
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-    transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))]
-)
+label_count = 3
 
-def load_train_data():
-    # type = train or test
-    transform = transforms.Compose([transforms.Resize([128,128]), transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
-    # ggf. transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) 
-    dataset = datasets.ImageFolder(f'../assets/train/',transform=transform)
+def load_data():
+    # Transforming Input into tensors for CNN usage
+    transform = transforms.Compose(
+        [transforms.Resize([128,128]),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
+
+    dataset = datasets.ImageFolder(f'../assets/no_undefined/',transform=transform)
     global dataset_index
     dataset_index = dataset.class_to_idx
-    return dataset
 
-def load_test_data():
-    # type = train or test
-    transform = transforms.Compose([transforms.Resize([128,128]),transforms.ToTensor()])
-    dataset = datasets.ImageFolder(f'../assets/test/',transform=transform)
-    return dataset
+    test_size = int(len(dataset)*0.1)
+    train_size = len(dataset)-test_size
+    print(f"Test Size: {test_size}; Train Size: {train_size}; Total in Dataset: {len(dataset)}")
+    train_set, val_set = torch.utils.data.random_split(dataset, [train_size, test_size], generator=torch.Generator().manual_seed(42))
+    return train_set, val_set
 
-#def custom_data_loader():
+def evaluate_model(model, device, test_loader, loss_function):
+    '''
+    model.eval() is a kind of switch for some specific layers/parts of the model that behave differently during training and inference (evaluating) time.
+    For example, Dropouts Layers, BatchNorm Layers etc.
+    You need to turn off them during model evaluation, and .eval() will do it for you.
+    In addition, the common practice for evaluating/validation is using torch.no_grad() in pair with model.eval() to turn off gradients computation
+    '''
+    loss_total = 0
+    model.eval()
 
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        n_class_correct = [0 for i in range(label_count)]
+        n_class_samples = [0 for i in range(label_count)]
 
-# use imshow for printing images for later use:
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+
+            loss = loss_function(outputs, labels)
+            loss_total += loss.item()
+
+            _, predicted = torch.max(outputs, 1)
+            n_samples = n_samples + labels.size(0)
+            n_correct = n_correct + (predicted == labels).sum().item()
+
+            for i in range(len(images)): #ehem. batch_size
+                label = labels[i]
+                pred = predicted[i]
+
+                if (label == pred):
+                    n_class_correct[label] = n_class_correct[label] + 1
+                
+                n_class_samples[label] = n_class_samples[label] + 1
+
+        acc = 100.0 * n_correct / n_samples
+        print(f'Accuracy of the network: {acc}%')
+        for i in range(label_count):
+            acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+            print(f'Accuracy of {label_to_string(i)}: {acc}%; {n_class_correct[i]}/{n_class_samples[i]} correct.')
+
+        return loss_total / len(test_loader)
 
 ## Doesn't work with tensor, only a raw dataset
 def show_img(d):
@@ -72,13 +105,13 @@ def show_img(d):
     plt.axis('off')
     plt.show()
 
-def conv_output_shape(h_w, kernel_size=1, stride=1, pad=0, dilation=1):
-    from math import floor
-    if type(kernel_size) is not tuple:
-        kernel_size = (kernel_size, kernel_size)
-    h = floor( ((h_w[0] + (2 * pad) - ( dilation * (kernel_size[0] - 1) ) - 1 )/ stride) + 1)
-    w = floor( ((h_w[1] + (2 * pad) - ( dilation * (kernel_size[1] - 1) ) - 1 )/ stride) + 1)
-    return h, w
+# def conv_output_shape(h_w, kernel_size=1, stride=1, pad=0, dilation=1):
+#     from math import floor
+#     if type(kernel_size) is not tuple:
+#         kernel_size = (kernel_size, kernel_size)
+#     h = floor( ((h_w[0] + (2 * pad) - ( dilation * (kernel_size[0] - 1) ) - 1 )/ stride) + 1)
+#     w = floor( ((h_w[1] + (2 * pad) - ( dilation * (kernel_size[1] - 1) ) - 1 )/ stride) + 1)
+#     return h, w
 
 def label_to_string(label):
     if label == 0:
@@ -108,7 +141,7 @@ class ConvNet(nn.Module):
         #Linear(Inputsize:Outputsize of laster layer * Kernel Size,Outputsize)
         self.fc1 = nn.Linear(64*29*29, 1024)
         self.fc2 = nn.Linear(1024, 256)
-        self.fc3 = nn.Linear(256, 4)
+        self.fc3 = nn.Linear(256, label_count)
 
          # Define proportion or neurons to dropout
         self.dropout = nn.Dropout(0.5)
@@ -130,16 +163,12 @@ class ConvNet(nn.Module):
 
 
 if __name__ == "__main__":
-    ### Try Runtime on CUDA ###
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print( f"device: {device}" )
-
     ### Load data ### 
-    train_dataset = load_train_data()
-    test_dataset = load_test_data()
+    train_dataset, test_dataset = load_data()
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    n_total_steps = len(train_loader)
 
     # Test for dataloading
     # images, labels = next(iter(train_loader))
@@ -153,22 +182,19 @@ if __name__ == "__main__":
     # exit(0)
 
     ### Setup for ConvModule ###
-
-    # classes = ('rock','paper','scissors','miscellaneous')
-
-
     model = ConvNet().to(device)
-    x = torch.randn(1, 3, 128, 128)
-
-    #summary(model,(3,128,128))
     summary(model, input_size=(batch_size, 3, 128, 128))
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    n_total_steps = len(train_loader)
+    
+    # Early stopping
+    last_loss = 100
+    patience = 2
+    trigger_times = 0
 
     for epoch in range(num_epochs):
+        t0 = time.time()
         for i, (images,labels) in enumerate(train_loader):
             images = images.to(device)
             labels = labels.to(device)
@@ -180,46 +206,28 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
-            if (i+1) % math.floor(n_total_steps/4) == 0 or (i+1) % n_total_steps == 0 or (i+1) % 25 == 0:
-                print(f'Epoch: [{epoch+1}/{num_epochs}],Step: [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
-        print(f'Epoch: [{epoch+1}/{num_epochs} finished.]')
+            if (i+1) % math.floor(n_total_steps/10) == 0 or (i+1) % n_total_steps == 0:
+                print(f'Epoch: [{epoch+1}/{num_epochs}],Step: [{i+1}/{n_total_steps}], Loss: {loss.item():.8f}')
+        print(f'Epoch: [{epoch+1}/{num_epochs} finished in {int(time.time() - t0)} seconds.]')
+        ### Evaluating + Early stopping the Model ###
+        current_loss = evaluate_model(model, device, test_loader, criterion)
+        print(f'The Current Loss: {current_loss}')
+        
+        if current_loss > last_loss:
+            trigger_times += 1
+            print(f'Trigger Times: {trigger_times}')
 
-    print('Finished Training')
+            if trigger_times >= patience:
+                print('Early stopping!')
+                break
+        else:
+            ### Setup path to save model ###
+            MODEL_PATH = './cnn.pth'
+            torch.save(model.state_dict(), MODEL_PATH)
+            # Reset Early Stopping Trigger
+            print(f'Trigger times back to 0.')
+            trigger_times = 0
 
-    ### Setup path to save model ###
+        last_loss = current_loss
 
-    PATH = './cnn.pth'
-    torch.save(model.state_dict(), PATH)
-
-    ### Evaluating the Model ###
-
-    with torch.no_grad():
-        n_correct = 0
-        n_samples = 0
-        n_class_correct = [0 for i in range(4)]
-        n_class_samples = [0 for i in range(4)]
-
-        for images, labels in test_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-
-            _, predicted = torch.max(outputs, 1)
-            n_samples = n_samples + labels.size(0)
-            n_correct = n_correct + (predicted == labels).sum().item()
-
-            for i in range(len(images)): #ehem. batch_size
-                label = labels[i]
-                pred = predicted[i]
-
-                if (label == pred):
-                    n_class_correct[label] = n_class_correct[label] + 1
-                
-                n_class_samples[label] = n_class_samples[label] + 1
-
-        acc = 100.0 * n_correct / n_samples
-        print(f'Accuracy of the network: {acc}%')
-
-        for i in range(4):
-            acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-            print(f'Accuracy of {label_to_string(i)}: {acc}%')
+    print('### Finished Training ###')
